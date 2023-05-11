b2bc93c17c377e845e50021fe37a7967b3edd95b
Yauheni Bykau <76494975+BukovY@users.noreply.github.com>
2023-05-12 00:26:36 +0300

    useful comment

--- src/components/CvViewer/CvViewer.tsx
+++ src/components/CvViewer/CvViewer.tsx
@@ -1,17 +1,24 @@
-import { ResumeViewerType } from './CvViewer.types';
+import { ResumeViewerType, Template } from './CvViewer.types';
 import { Basic } from './Templates/Basic/Basic';
 import { Modern } from './Templates/Modern/Modern';
+import { ModernColumns } from './Templates/ModernColumns/ModernColumns';
 
 type CvViewerProps = {
   cv: ResumeViewerType;
 };
 export const CvViewer = ({ cv }: CvViewerProps) => {
   switch (cv.template) {
-    case 'basic reverse':
+    case Template.Basic:
       return <Basic cv={cv} isReverse />;
-    case 'modern':
+    case Template.Modern:
       return <Modern cv={cv} />;
+    case Template.ModernBlack:
+      return <Modern cv={cv} isBlack />;
+    case Template.ModernColumns:
+      return <ModernColumns cv={cv} />;
+    case Template.ModernColumnBlack:
+      return <ModernColumns cv={cv} isBlack />;
     default:
-      return <Basic cv={cv} isReverse={false} />;
+      return <Basic cv={cv} />;
   }
 };
