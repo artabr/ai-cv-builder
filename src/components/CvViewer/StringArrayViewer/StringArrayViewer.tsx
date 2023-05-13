17e029ca6b8b6318369ccdf32144aa75afa5b169
Yauheni Bykau <76494975+BukovY@users.noreply.github.com>
2023-05-13 12:28:27 +0300

    useful comment

--- src/components/CvViewer/StringArrayViewer/StringArrayViewer.tsx
+++ src/components/CvViewer/StringArrayViewer/StringArrayViewer.tsx
@@ -1,17 +1,13 @@
-import css from './StringArrayViewer.module.css';
-
 type HobbiesProps = {
   items?: string[];
 };
 
 export const StringArrayViewer = ({ items = [] }: HobbiesProps) => {
   return (
-    <div>
+    <>
       {items.map((el) => (
-        <h5 className={css.title} key={el}>
-          {el}
-        </h5>
+        <h5 key={el}>{el}</h5>
       ))}
-    </div>
+    </>
   );
 };
